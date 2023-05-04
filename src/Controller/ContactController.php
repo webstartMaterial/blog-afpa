bd808d0a89717a6143537d36f69e415ffb83ca96
Samih Habbani Pro 16 <web.start.contact@gmail.com>
2023-05-04 09:55:37 +0200

    mise en place formulaire de contact + msg flash + insert en bdd

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+
+        // créer le formulaire à partir de l'instance de la classe Contact
+        // car mon formulaire est lié à la classe Contact
+        // la classe contact est instanciée car je peux initialiser des valeurs à mes champs
+
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $contact = $form->getData();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+            
+            $this->addFlash('confirmation', 'Votre email a bien été envoyé !');
+
+            // return $this->redirectToRoute('task_success');
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'contact_form' => $form,
         ]);
+
     }
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => array(
+                    'placeholder' => 'Entrer votre prénom'
+                )
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+                'attr' => array(
+                    'placeholder' => 'Entrer votre nom'
+                )
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => array(
+                    'placeholder' => 'Entrer votre email'
+                )
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message',
+                'attr' => array(
+                    'placeholder' => 'Entrer votre message ici'
+                )
+            ])
+            ->add('object', ChoiceType::class, [
+                'label' => 'Que nous vaut le plaisir?',
+                'choices' => [
+                    'Je souhaite vous contacter par choix 1' => 'Choix 1',
+                    'Je souhaite vous contacter par choix 2' => 'Choix 2' ,
+                    'Je souhaite vous contacter par choix 3' => 'Choix 3'
+                ]
+            ])
+            ->add('phone', NumberType::class, [
+                'label' => 'Votre numéro de téléphone',
+                'attr' => array(
+                    'placeholder' => 'Entrer votre numéro de téléphone'
+                )
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'save btn-primary'],
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
